520728bfeaa1ba792ee69e956526e0a51f28e719
Bart Noordsij <bart.noordsij@gmail.com>
2026-09-14 09:44:19 +0200

    added test endpoint for http.get

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"io"
 	"net/http"
-
+	"os"
 	"github.com/syumai/workers-go"
 )
 
@@ -12,6 +12,24 @@ func main() {
 
 	http.HandleFunc("/status", statusHandler)
 	http.HandleFunc("/validate", validateHandler)
+
+        http.HandleFunc("/tst-example", func(w http.ResponseWriter, req *http.Request) {
+                out, err := os.Create("output.txt")
+		if err != nil {
+ 			panic(err)
+		}
+		defer out.Close()
+
+                resp, err := http.Get("http://example.com/")
+		if err != nil {
+ 			panic(err)
+		}
+                defer resp.Body.Close()
+
+                io.Copy(out, resp.Body)
+
+        })
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		msg := "Hello!"
 		w.Write([]byte(msg))
